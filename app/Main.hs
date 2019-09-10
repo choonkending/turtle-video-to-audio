5ccb6b91b9ef5c2de766b40ebb2f9cf09e685c52
Ken Ding <aenkil@gmail.com>
2019-09-11 07:57:28 +1000

    Print mp4 files within a certain directory

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,16 @@
 module Main where
 
 import Turtle (Text, empty)
-import Turtle.Prelude (shell)
+import Turtle.Prelude (shell, find)
 import Turtle.Line (Line)
+import Turtle.Shell (view)
+import Turtle.Pattern (suffix)
 
 command :: Text
 command = "ffmpeg -i a.mp4 -q:a 0 -map a a.mp3"
 
+runCommand :: IO ()
+runCommand = shell command empty >>= (\exitCode -> putStrLn $ show exitCode)
+
 main :: IO ()
-main = shell command empty >>= (\exitCode -> putStrLn $ show exitCode)
+main = view (find (suffix ".mp4") "/Users/Ken/Downloads")
