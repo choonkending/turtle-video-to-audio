45196e6d90c0c10ba13d2d12a0404bdea3c4ec46
Ken Ding <aenkil@gmail.com>
2019-09-12 20:58:53 +1000

    Run commands

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,37 @@
 
 module Main where
 
-import Turtle (Text, empty)
-import Turtle.Prelude (shell, find)
-import Turtle.Line (Line)
-import Turtle.Shell (view)
+import qualified Turtle
+import qualified Control.Foldl as L
+import Turtle.Prelude (find, shell)
+import Turtle.Shell (Shell, fold)
 import Turtle.Pattern (suffix)
+import Turtle.Format (format, s, (%))
+import Data.Either (either)
+import Data.Maybe (maybe)
+import Data.Text (stripSuffix)
 
-command :: Text
-command = "ffmpeg -i a.mp4 -q:a 0 -map a a.mp3"
+formatFilePath :: Turtle.Text -> Turtle.Text
+formatFilePath file = format ("ffmpeg -i \""%s%".mp4\" -q:a 0 -map a \""%s%".mp3\"") file file
 
-runCommand :: IO ()
-runCommand = shell command empty >>= (\exitCode -> putStrLn $ show exitCode)
+stripMp4Suffix :: Turtle.Text -> Turtle.Text
+stripMp4Suffix file = maybe file id (stripSuffix ".mp4" file)
+
+ifSuccess :: Turtle.Text -> Turtle.Text
+ifSuccess = formatFilePath . stripMp4Suffix
+
+ifFailure :: Turtle.Text -> Turtle.Text
+ifFailure = id
+
+createCommand :: Turtle.FilePath -> Turtle.Text
+createCommand filePath = either ifFailure ifSuccess (Turtle.toText filePath)
+
+getMP4Files :: Shell Turtle.FilePath
+getMP4Files = find (suffix ".mp4") "/Users/Ken/Downloads/test"
+
+runCommand :: Turtle.Text -> IO ()
+runCommand command = shell command Turtle.empty >>= print
 
 main :: IO ()
-main = view (find (suffix ".mp4") "/Users/Ken/Downloads")
+main = fold (createCommand <$> getMP4Files) L.list >>=
+  \listOfCommands -> L.fold (L.Fold (\_ -> runCommand) (return ()) id) listOfCommands
