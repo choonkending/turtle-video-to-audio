309581eb978db2760d71af3f7f620a70888274de
Ken Ding <aenkil@gmail.com>
2019-09-14 23:03:27 +1000

    Modify Main.hs to use proc
    - Also reduce complexity of code by using format fp

--- app/Main.hs
+++ app/Main.hs
@@ -4,34 +4,30 @@ module Main where
 
 import qualified Turtle
 import qualified Control.Foldl as L
-import Turtle.Prelude (find, shell)
+import Turtle.Prelude (find, proc)
 import Turtle.Shell (Shell, foldIO)
 import Turtle.Pattern (suffix)
-import Turtle.Format (format, s, (%))
-import Data.Either (either)
+import Turtle.Format (format, fp)
 import Data.Maybe (maybe)
 import Data.Text (stripSuffix)
-
-formatFilePath :: Turtle.Text -> Turtle.Text
-formatFilePath file = format ("ffmpeg -i \""%s%".mp4\" -q:a 0 -map a \""%s%".mp3\"") file file
 
 stripMp4Suffix :: Turtle.Text -> Turtle.Text
 stripMp4Suffix file = maybe file id (stripSuffix ".mp4" file)
 
-ifSuccess :: Turtle.Text -> Turtle.Text
-ifSuccess = formatFilePath . stripMp4Suffix
+createOutputFileName :: Turtle.Text -> Turtle.Text
+createOutputFileName filePath = (stripMp4Suffix filePath) <> ".mp3"
 
-ifFailure :: Turtle.Text -> Turtle.Text
-ifFailure = id
-
-createCommand :: Turtle.FilePath -> Turtle.Text
-createCommand filePath = either ifFailure ifSuccess (Turtle.toText filePath)
+createArguments :: Turtle.FilePath -> [Turtle.Text]
+createArguments filePath =
+  let inputFileName = format fp filePath
+      outputFileName = createOutputFileName inputFileName
+  in ["-i", inputFileName, "-q:a", "0",  "-map", "a", outputFileName]
 
 getMP4Files :: Shell Turtle.FilePath
 getMP4Files = find (suffix ".mp4") "/Users/Ken/Downloads/test"
 
-runCommand :: Turtle.Text -> IO ()
-runCommand command = shell command Turtle.empty >>= print
+runCommand :: Turtle.FilePath -> IO ()
+runCommand filePath = proc "ffmpeg" (createArguments filePath) Turtle.empty >>= print
 
 main :: IO ()
-main = foldIO (createCommand <$> getMP4Files) (L.sink runCommand)
+main = foldIO getMP4Files (L.sink runCommand)
