bb50b0fa7c8d7b708c35cda9225c486c9af0c221
Ken Ding <aenkil@gmail.com>
2020-08-14 18:11:45 +1000

    refactor: Modify Stitch file to refer to default config

--- app/Stitch.hs
+++ app/Stitch.hs
@@ -6,38 +6,22 @@ import qualified Turtle
 import qualified Control.Foldl as L
 import Turtle.Prelude (find, proc)
 import Turtle.Shell (Shell, foldIO)
-import Turtle.Pattern (suffix, has, text, Pattern)
+import Turtle.Pattern (suffix)
 import Turtle.Format (format, fp)
-import Data.Maybe (fromMaybe)
-import Data.Text (stripSuffix)
-import Data.Map (fromList, findWithDefault)
-
-introTemplateMap = fromList([("nbm", "INTRO.mp3"), ("bsv", "ONSITE.mp3")])
-
-getIntroFile :: Turtle.Text -> Pattern Turtle.Text
-getIntroFile fileName = text (findWithDefault "ONSITE.mp3" fileName introTemplateMap)
-
-stripMp3Suffix :: Turtle.Text -> Turtle.Text
-stripMp3Suffix file = fromMaybe file (stripSuffix ".mp3" file)
-
-createOutputFileName :: Turtle.Text -> Turtle.Text
-createOutputFileName filePath = stripMp3Suffix filePath <> "-final.mp3"
+import Config (defaultConfig, editInputFilesDirectory, editOutputFilesDirectory, editTemplatesDirectory, introFileName)
 
 getMP3Files :: Shell Turtle.FilePath
-getMP3Files = find (suffix ".mp3") "/Users/Ken/Downloads/convert/stitch"
+getMP3Files = find (suffix ".mp3") $ Turtle.fromText $ editInputFilesDirectory defaultConfig
 
-getOpeningFile :: Shell Turtle.FilePath
-getOpeningFile = find (has (getIntroFile "nbm")) "/Users/Ken/Documents/Buddhist/Editing Templates"
-
-createArguments :: Turtle.FilePath -> Turtle.FilePath -> [Turtle.Text]
-createArguments openingFilePath mainFilePath =
-  let openingFileName = format fp openingFilePath
+createArguments :: Turtle.FilePath -> [Turtle.Text]
+createArguments mainFilePath =
+  let openingFileName = editTemplatesDirectory defaultConfig <> introFileName defaultConfig
       mainFileName = format fp mainFilePath
-      outputFileName = createOutputFileName mainFileName
+      outputFileName = editOutputFilesDirectory defaultConfig <> format fp (Turtle.filename mainFilePath)
   in ["-i", openingFileName, "-i", mainFileName, "-filter_complex", "concat=n=2:v=0:a=1", outputFileName]
 
-runCommand :: (Turtle.FilePath, Turtle.FilePath) -> IO ()
-runCommand (f1, f2) = proc "ffmpeg" (createArguments f1 f2) Turtle.empty >>= print
+runCommand :: Turtle.FilePath -> IO ()
+runCommand filePath = proc "ffmpeg" (createArguments filePath) Turtle.empty >>= print
 
 stitch :: IO ()
-stitch = foldIO (Turtle.liftA2 (,) getOpeningFile getMP3Files) (L.sink runCommand)
+stitch = foldIO getMP3Files (L.sink runCommand)
